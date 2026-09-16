4209693174ebc18ff3582501cadfbd2dde9b2725
sivavijay0805 <gorthisivavijay@gmail.com>
2026-09-16 23:23:10 +0530

    Add greeting message and user input functionality

---
+++ app.py
@@ -0,0 +1,8 @@
+# Print a simple message
+print("Hello, World!")
+
+# Take input from the user
+user_name = input("Enter your name: ")
+
+# Display a dynamic greeting using an f-string
+print(f"Welcome to Python, {user_name}!")
